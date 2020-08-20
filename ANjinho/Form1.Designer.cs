13a3f603c98352f662303f626aa7bdf58d55d7bc
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2020-08-20 01:57:32 -0300

    Add files via upload

---
+++ ANjinho/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ANjinho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.HEADER = new System.Windows.Forms.Panel();
+            this.TextoAnjin = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Conectar = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.HEADER.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // HEADER
+            // 
+            this.HEADER.BackColor = System.Drawing.Color.DimGray;
+            this.HEADER.Controls.Add(this.TextoAnjin);
+            this.HEADER.ForeColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.HEADER, "HEADER");
+            this.HEADER.Name = "HEADER";
+            // 
+            // TextoAnjin
+            // 
+            resources.ApplyResources(this.TextoAnjin, "TextoAnjin");
+            this.TextoAnjin.Name = "TextoAnjin";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // Conectar
+            // 
+            this.Conectar.BackColor = System.Drawing.Color.Orange;
+            resources.ApplyResources(this.Conectar, "Conectar");
+            this.Conectar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Conectar.Name = "Conectar";
+            this.Conectar.UseVisualStyleBackColor = false;
+            this.Conectar.Click += new System.EventHandler(this.Conectar_Click);
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Conectar);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.HEADER);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.HEADER.ResumeLayout(false);
+            this.HEADER.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel HEADER;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label TextoAnjin;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ANjinho/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANjinho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BotaoConectar_Click(object sender, EventArgs e)
+        {
+            Process.Start("fivem://connect/200.9.154.118");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("chrome.exe");
+        }
+
+        private void DeletarPrefetch_Click(object sender, EventArgs e)
+        {
+            Directory.Delete(@"C:\Windows\Prefetch\", true);
+        }
+
+        private void FivemClose_Click(object sender, EventArgs e)
+        {
+            var processoFechar = Process.GetProcessesByName("chrome.exe");
+            foreach (Process opp in processoFechar)
+            {
+                opp.Kill();
+            }
+        }
+
+        private void Conectar_Click(object sender, EventArgs e)
+        {
+            Process.Start("fivem://connect/200.9.154.118");
+        }
+
+        private void panel3_Paint_1(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+                    }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
